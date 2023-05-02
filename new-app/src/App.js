ddf0f3d40ed3488f1c7eb5813f488449cbf0dcf8
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-05-02 19:55:30 +0300

    updated

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [number, setNumber] = useState(0);
+  const [name, setName] = useState("Emir");
+
+  useEffect(() => {
+    console.log("Component Mount Edildi");
+  }, []);
+  
+  useEffect(() => {
+    console.log("number State Güncellendi");
+  },[number]);
+  
+  useEffect(() => {
+    console.log("name State Güncellendi");
+  },[name]);
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>{number}</h1>
+    <button onClick={() => setNumber(number+1)} >+</button>
+
+    <hr />
+
+    <h1>{name}</h1>
+    <button onClick={() => setName("İsmail")} >Change Name</button>
     </div>
   );
 }
