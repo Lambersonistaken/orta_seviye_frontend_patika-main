13742d87a3303a093f49f240945bcba35a3142bb
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-05-10 21:22:02 +0300

    added some functionalities

--- para_harcama_uygulamasi/src/App.js
+++ para_harcama_uygulamasi/src/App.js
@@ -7,13 +7,22 @@ function App() {
 
   const [money, setMoney] = useState(100);
   const [basket, setBasket] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => { 
+    const t = basket.reduce((acc, item) => {
+      return (acc + (item.amount * (products.find(product => product.id === item.id).price))); // toplam fiyat hesaplanıyor
+    }
+    , 0);
+    console.log(t);
+  }, [basket]);
 
   return (
     <>
     <Header money={money}/>
     {
       products.map(product => (
-        <Product basket={basket} setBasket={setBasket} product={product}/>  
+        <Product key={product.id} basket={basket} setBasket={setBasket} product={product}/>  
       ))
     }
     </>
--- para_harcama_uygulamasi/src/components/Product.jsx
+++ para_harcama_uygulamasi/src/components/Product.jsx
@@ -2,15 +2,46 @@ import React from 'react'
 import styles from '../styles/product.module.css'
 
 function Product({product, basket, setBasket}) {
+
+    const basketItem = basket.find(item => item.id === product.id) // ürün daha önce eklenmiş mi
+
+    const addBasket = () => {
+        const checkBasket = basket.find(item => item.id === product.id)
+        // ürün daha önce eklenmiş
+        if(checkBasket){
+            checkBasket.amount += 1
+            setBasket([...basket.filter(item => item.id !== product.id),checkBasket])
+           
+        }else{
+           setBasket([...basket,{
+                id:product.id,
+                amount:1
+           }])
+        }
+
+    }
+
+    const removeBasket = () => {
+        const currentBasket = basket.find(item => item.id === product.id) // ürün daha önce eklenmiş mi
+        const basketWithoutCurrent = basket.filter(item => item.id !== product.id)
+        currentBasket.amount -= 1
+        if(currentBasket.amount === 0){
+            setBasket([...basketWithoutCurrent])
+        }else{
+            setBasket([...basketWithoutCurrent,currentBasket])
+        }
+    }
+
+
   return (
     <>
     <div className={styles.product}>
     <h6>{product.title}</h6>
     <div className="price">$ {product.price}</div>
     <div className="actions">
-        <button>Sat</button>
-        <span className="amount">0</span>
-        <button>Satın Al</button>
+        <button onClick={removeBasket} disabled={!basketItem}>Sat</button>
+        <span className="amount">{basketItem && basketItem.amount || 0 }</span>
+        <button onClick={addBasket}>Satın Al</button>
     </div>
     </div>
     
