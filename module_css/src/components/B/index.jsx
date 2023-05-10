f55ecc51fa4a2124ac36b9ad179bbcc13b7c8397
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-05-10 18:18:42 +0300

    learning module css concept

---
+++ module_css/src/components/B/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './style.module.css'
+
+function index() {
+  return (
+    <div style={styles.title}>
+      B
+    </div>
+  )
+}
+
+export default index
