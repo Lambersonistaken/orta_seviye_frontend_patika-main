baea9147ae8b575d568c02e790a9c7e164b8fca7
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-05-13 01:04:29 +0300

    loading functionality added

--- native-fetch/src/components/Users.jsx
+++ native-fetch/src/components/Users.jsx
@@ -3,12 +3,14 @@ import {useState,useEffect} from 'react'
 function Users() {
 
     const [users, setUsers] = useState([])
+    const [loading, setLoading] = useState(true)
 
 
    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
         .then(json => setUsers(json) )
+        .finally(() => setLoading(false))
     }, [])
 
 
@@ -16,6 +18,9 @@ function Users() {
   return (
     <div>
         <h1>Users</h1>
+
+        {loading ? <h2>Loading...</h2> : null}
+
         {users.map((user) => {
             return (
                 <div key={user.id}>
