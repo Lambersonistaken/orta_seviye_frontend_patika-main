fc4a881b404083b4cf9aa2f0faa750e50b8486b1
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-05-16 14:51:25 +0300

    routing learning

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,11 +1,49 @@
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+import Users from "./components/Users";
+import User from "./components/User";
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+            <li>
+            <Link to="/user">User</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/user" element={<User />} /> 
+          <Route path="/" element={<Home />} /> 
+          
+          
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
 
 export default App;
