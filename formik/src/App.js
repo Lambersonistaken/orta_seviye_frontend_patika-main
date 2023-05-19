1939677a45d6f9753661248a8fbb76a8464606da
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-05-20 01:23:56 +0300

    formik Radio / Checkbox / Dropdown

--- formik/src/App.js
+++ formik/src/App.js
@@ -14,13 +14,16 @@ function App() {
         firstName: '',
         lastName: '',
         email: '',
+        gender: 'male',
+        hobbies: [],
+        country: 'Turkey'
       }}
       onSubmit={async (values) => {
-        alert(values.firstName + " " + values.lastName + " " + values.email)
+        alert(values.firstName + " " + values.lastName + " " + values.email + " " + values.gender)
       }}
     >
       {
-        ({handleSubmit, handleChange}) => (
+        ({handleSubmit, handleChange, values}) => (
           <form onSubmit={handleSubmit}>
         <label htmlFor="firstName">First Name</label>
         <input id="firstName" name="firstName" placeholder="Jane"
@@ -45,7 +48,52 @@ function App() {
           type="email"
           onChange={handleChange}
         />
+
+        <br />
+        <br />
+          <span>Male</span>
+          <input type='radio' name='gender' value="male" onChange={handleChange} checked={values.gender === "male"}/>
+          <span>Female</span>
+          <input type='radio' name='gender' value="female" onChange={handleChange} />
+          <br />
+          <br />
+
+          <div>
+            Football
+            <input type="checkbox" name='hobbies' value='Play Football' onChange={handleChange} />
+          </div>
+
+          <div>
+            Cinema
+            <input type="checkbox" name='hobbies' value='Cinema' onChange={handleChange} />
+          </div>
+
+          <div>
+            Hiking
+            <input type="checkbox" name='hobbies' value='Hiking' onChange={handleChange} />
+          </div>
+          <br />
+
+
+          <select name="country" id="" onChange={handleChange}>
+            <option value="Turkey">Turkey</option>
+            <option value="Italy">Italy</option>
+            <option value="Germany">Germany</option>
+          </select>
+
+          <br />
+
+          <br />
+          
+
         <button type="submit">Submit</button>
+        <br />
+        <br />
+
+          <code>
+            {JSON.stringify(values, null, 2)}
+          </code>
+
       </form> 
         )
       }
