c6d6f3ff58a4708231ed5a76e985ab231c9fe663
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-05-24 15:36:38 +0300

    useMemo

--- react-memo/src/App.jsx
+++ react-memo/src/App.jsx
@@ -1,19 +1,27 @@
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
 import './App.css'
 import Header from './components/Header'
 
 function App() {
   const [number, setNumber] = useState(0)
 
-  return (
+  const data = useMemo(() => {
+    return {
+      name: "Emir",
+    }
+  }, [])
+
+    return (
     <>
-      <Header number={number < 5 ? 0 : number}/> {/* header componenti number 5 olana kadar re-render edilmez 5 olduktan sonra edilir. içindeki proplar değişince re-render edilir. */}
+
+      <Header number={number < 5 ? 0 : number} data={data}/> {/* header componenti number 5 olana kadar re-render edilmez 5 olduktan sonra edilir. içindeki proplar değişince re-render edilir. */}
 
       <hr />
       <h1>{number}</h1>
       <button onClick={() => setNumber(number+1)}>ARTTIR</button>
     </>
-  )
+  );
 }
+
 
 export default App
--- react-memo/src/components/Header.jsx
+++ react-memo/src/components/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
-function Header({number}) {
+function Header({number,data}) {
     console.log("Header component re-rendered");
   return (
     <div>
       <h1 className="head">Counter App - {number}</h1>
+
+      <br />
+      <br />
+
+      <code>{JSON.stringify(data)}</code>
+
     </div>
   )
 }
