040d642bd02fade547aaeecd49870f56a58e0ab8
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-07-05 17:54:56 +0300

    components created

--- redux-todo-app/src/components/Content.jsx
+++ redux-todo-app/src/components/Content.jsx
@@ -1,35 +1,17 @@
 import React from "react";
+import TodoList from "./TodoList";
+import ContentFooter from "./ContentFooter";
 
 function Content() {
   return (
-    <section className="main">
-      <input className="toggle-all" type="checkbox" />
-      <label for="toggle-all">Mark all as complete</label>
-
-      <ul className="todo-list">
-        <li className="completed">
-          <div className="view">
-            <input className="toggle" type="checkbox" />
-            <label>Learn JavaScript</label>
-            <button className="destroy"></button>
-          </div>
-        </li>
-        <li>
-          <div className="view">
-            <input className="toggle" type="checkbox" />
-            <label>Learn React</label>
-            <button className="destroy"></button>
-          </div>
-        </li>
-        <li>
-          <div className="view">
-            <input className="toggle" type="checkbox" />
-            <label>Have a life!</label>
-            <button className="destroy"></button>
-          </div>
-        </li>
-      </ul>
-    </section>
+    <>
+      <section className="main">
+        <input className="toggle-all" type="checkbox" />
+        <label htmlFor="toggle-all">Mark all as complete</label>
+        <TodoList />
+      </section>
+      <ContentFooter />
+    </>
   );
 }
 
