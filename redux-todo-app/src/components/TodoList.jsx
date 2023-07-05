f668616f68311205efb9e163719c3118f3133272
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-07-05 18:58:49 +0300

    todolist updated

--- redux-todo-app/src/components/TodoList.jsx
+++ redux-todo-app/src/components/TodoList.jsx
@@ -1,29 +1,23 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleTodo, deleteTodo, addTodo } from "../redux/Todos/todosSlice";
 
 function TodoList() {
+  const todos = useSelector((state) => state.todos);
+  console.log(todos);
+  const dispatch = useDispatch();
+
   return (
     <ul className="todo-list">
-      <li className="completed">
-        <div className="view">
-          <input className="toggle" type="checkbox" />
-          <label>Learn JavaScript</label>
-          <button className="destroy"></button>
-        </div>
-      </li>
-      <li>
-        <div className="view">
-          <input className="toggle" type="checkbox" />
-          <label>Learn React</label>
-          <button className="destroy"></button>
-        </div>
-      </li>
-      <li>
-        <div className="view">
-          <input className="toggle" type="checkbox" />
-          <label>Have a life!</label>
-          <button className="destroy"></button>
-        </div>
-      </li>
+      {todos.map((todo) => (
+        <li key={todo.id} className={todo.completed ? "completed" : "view"}>
+          <div>
+            <input className="toggle" type="checkbox" />
+            <label>{todo.title}</label>
+            <button className="destroy"></button>
+          </div>
+        </li>
+      ))}
     </ul>
   );
 }
