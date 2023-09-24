34033ba4e2d20b9f20a862d5384fed1bffd16b1f
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-09-24 03:15:44 +0300

    header done

---
+++ bill-gates-money/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div>
+      <div className="header_container">
+        <h1>Bill Gates Money Clone</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
