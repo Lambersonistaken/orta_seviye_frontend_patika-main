e04e948edbc7aeef779d2c8a07f4e89308256982
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-09-24 04:05:01 +0300

    finished

--- bill-gates-money/src/components/MainBody.jsx
+++ bill-gates-money/src/components/MainBody.jsx
@@ -6,7 +6,7 @@ function MainBody() {
 
   let isSpent = false;
 
-  if (money === "$100,000,000,000") {
+  if (money === 10000000000) {
     isSpent = false;
   } else {
     isSpent = true;
@@ -25,20 +25,37 @@ function MainBody() {
           <h2>$2</h2>
           <div className="buttons">
             <div>
-              <button className="sell_btn" disabled={!isSpent}>
+              <button
+                className="sell_btn"
+                onClick={() => {
+                  const currentMoney1 = parseInt(money.replace(/\D/g, ""), 10);
+                  const newMoney1 = currentMoney1 + 2;
+                  setMoney("$" + newMoney1.toLocaleString());
+                }}
+                disabled={!isSpent}
+              >
                 Sell
               </button>
             </div>
             <div>
-              <button className="buy_btn">Buy</button>
+              <button
+                className="buy_btn"
+                onClick={() => {
+                  const currentMoney1 = parseInt(money.replace(/\D/g, ""), 10);
+                  const newMoney1 = currentMoney1 - 2;
+                  setMoney("$" + newMoney1.toLocaleString());
+                }}
+              >
+                Buy
+              </button>
             </div>
           </div>
         </div>
 
         <div class="grid-item">
           <img src="https://neal.fun/spend/images/flip-flops.jpg" alt="" />
-          <h3>Big Mac</h3>
-          <h2>$2</h2>
+          <h3>Flip Flops</h3>
+          <h2>$10</h2>
           <div className="buttons">
             <div>
               <button className="sell_btn" disabled={!isSpent}>
@@ -53,8 +70,8 @@ function MainBody() {
 
         <div class="grid-item">
           <img src="https://neal.fun/spend/images/coca-cola-pack.jpg" alt="" />
-          <h3>Big Mac</h3>
-          <h2>$2</h2>
+          <h3>Coca Cola Pack</h3>
+          <h2>$12</h2>
           <div className="buttons">
             <div>
               <button className="sell_btn" disabled={!isSpent}>
@@ -69,8 +86,8 @@ function MainBody() {
 
         <div class="grid-item">
           <img src="https://neal.fun/spend/images/movie-ticket.jpg" alt="" />
-          <h3>Big Mac</h3>
-          <h2>$2</h2>
+          <h3>Movie Ticket</h3>
+          <h2>$5</h2>
           <div className="buttons">
             <div>
               <button className="sell_btn" disabled={!isSpent}>
@@ -85,8 +102,8 @@ function MainBody() {
 
         <div class="grid-item">
           <img src="https://neal.fun/spend/images/book.jpg" alt="" />
-          <h3>Big Mac</h3>
-          <h2>$2</h2>
+          <h3>Book</h3>
+          <h2>$20</h2>
           <div className="buttons">
             <div>
               <button className="sell_btn" disabled={!isSpent}>
@@ -101,8 +118,8 @@ function MainBody() {
 
         <div class="grid-item">
           <img src="https://neal.fun/spend/images/lobster-dinner.jpg" alt="" />
-          <h3>Big Mac</h3>
-          <h2>$2</h2>
+          <h3>Lobster Dinner</h3>
+          <h2>$200</h2>
           <div className="buttons">
             <div>
               <button className="sell_btn" disabled={!isSpent}>
